2b66ba5b29b8c8569f2d953dcb71ff0ac86aa978
PatriContreras <patri.contreras91@gmail.com>
2021-04-16 13:58:52 +0200

    Launching of the project Created routing module and basic paths Created basic components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { EventsComponent } from './components/events/events.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'portfolio' },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'aboutus', component: AboutusComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: PortfolioComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { EventsComponent } from './components/events/events.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    AboutusComponent,
+    EventsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
