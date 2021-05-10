d50135cb06c201437cd812cc25b659d009df4a56
PatriContreras <patri.contreras91@gmail.com>
2021-05-10 12:03:48 +0200

    Init upload image

--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -1,12 +1,12 @@
 export interface Event {
-    title: string,
-    place: string,
-    image: string,
-    model: string,
-    makeup: string,
-    price: number,
-    date: Date,
-    time: string,
-    description: string,
-    complete: boolean
+    title?: string,
+    place?: string,
+    image?: string,
+    model?: string,
+    makeup?: string,
+    price?: number,
+    date?: Date,
+    time?: string,
+    description?: string,
+    complete?: boolean
 }
