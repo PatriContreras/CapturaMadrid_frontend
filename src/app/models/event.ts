ac4fc6b1ea872ee7dd29e621c04a721e845d6131
PatriContreras <patri.contreras91@gmail.com>
2021-05-10 10:41:59 +0200

    Created event interface

---
+++ src/app/models/event.ts
@@ -0,0 +1,12 @@
+export interface Event {
+    title: string,
+    place: string,
+    image: string,
+    model: string,
+    makeup: string,
+    price: number,
+    date: Date,
+    time: string,
+    description: string,
+    complete: boolean
+}
